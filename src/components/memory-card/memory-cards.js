42632007bef98ed05afd29c12d1b21d317b5e91b
junioralves <jrnalves@gmail.com>
2019-07-04 16:22:01 -0300

    Aula 26 - Resoluçao dos Desafios, JSON, Object Destructuring, CSS

--- src/components/memory-card/memory-cards.js
+++ src/components/memory-card/memory-cards.js
@@ -1,4 +1,4 @@
-const createMemoryCard = (src, alt, nameClass) => `
+const createMemoryCard = ({nameClass, src, alt}) => `
     <article class="memory-card ${nameClass}">
         <img
             src="${src}"
@@ -11,64 +11,3 @@ const createMemoryCard = (src, alt, nameClass) => `
 `;
 
 const handleClick = () => console.log('AE');
-
-// const createMemoryCard = () => (`
-//         <article class="memory-card">
-//             <img
-//                 src='img/icon-collabcode.png'
-//                 alt='Gueio mascote da CollabCode'
-//                 class='icon'
-//                 onClick="handleClick()"
-//             />
-//         </article>    
-//     `);
-
-// const createMemoryCardFront = () => (`
-//         <article class="memory-card -front">
-//             <img
-//                 src='img/icon-c.png'
-//                 alt='Icone da linguagem C++'
-//                 class='icon'
-//                 onClick = "handleClick()"
-//             />
-//         </article>
-//     `);
-
-
-// function handleClick() {
-//     console.log('testeClick');
-// }
-
-
-
-// function createMemoryCard() {
-    
-//     const $memoryCard = `
-//         <article class="memory-card">
-//             <img
-//                 src='img/icon-collabcode.png'
-//                 alt='Gueio mascote da CollabCode'
-//                 class='icon'
-//                 onClick="handleClick()"
-//             />
-//         </article>    
-//     `;
-
-//     return $memoryCard;
-// }
-
-// function createMemoryCardFront() {
-    
-//     const $memoryCardFront = `
-//         <article class="memory-card -front">
-//             <img
-//                 src='img/icon-c.png'
-//                 alt='Icone da linguagem C++'
-//                 class='icon'
-//                 onClick = "handleClick()"
-//             />
-//         </article>
-//     `;
-    
-//     return $memoryCardFront
-// }
--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,17 +1,38 @@
 const $root = document.querySelector('#root');
 
 const $cardsWrapper = createCardsWrapper();
-const $memoryCard = createMemoryCard("img/icon-collabcode.png", "Gueio Mascote da CollabCode");
-const $memoryCardFront = createMemoryCard("img/icon-c.png", "Icone do livro C++", "-front");
 
+const $memoryCardC = createMemoryCard({
+    nameClass: "-front",
+    src: "img/icon-c.png",
+    alt: "Livro de programação C++"
+});
 
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
+const $memoryCardJS = createMemoryCard({
+    nameClass: "-front",
+    src: "img/icon-js.png",
+    alt: "Livro de programação JavaScript"
+});
+
+const $memoryCardPHP = createMemoryCard({
+    nameClass: "-front",
+    src: "img/icon-php.png",
+    alt: "livro de programação PHP"
+});
+
+const $memoryCardJava = createMemoryCard({
+    nameClass: "-front",
+    src: "img/icon-java.png",
+    alt: "livro de programação JAVA"
+});
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardPHP);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardPHP);
 
 $root.insertAdjacentElement("beforeend", $cardsWrapper);
