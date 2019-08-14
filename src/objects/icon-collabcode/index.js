e88c12d5952bd4cf7765289cfc8a19917913fa83
Junior <jrnalves@gmail.com>
2019-08-14 15:42:09 -0300

    Aula 39 - Desafio Basico - Inserido o icone da collabCode dentro de uma div e aplicado estilização com css

--- src/objects/icon-collabcode/index.js
+++ src/objects/icon-collabcode/index.js
@@ -2,10 +2,35 @@ const iconCollabCode = (function () {
 
     const module = {};
 
+    module._style = () => {
+        const $head = document.querySelector('head');
+        const $style = document.createElement('style');
+
+        $style.textContent = `
+            .bkpLogo {
+                width: 252px;
+                height: 252px;
+                background-color: #fff;
+                border-radius: 50%;
+            }
+
+            .icon {
+                width: 175px;
+                height: 175px;
+                margin: 50%;
+                transform: translate(-50%, -50%);
+            }
+        `;
+
+        $head.insertBefore($style, null);
+    }
+
     module.render = () => {
+        module._style();
+
         return `
-            <div>
-                <img src="img/icon-collabcode.png" alt="Icon CollabCode">
+            <div class="bkpLogo">
+                <img src="img/icon-collabcode.png" class="icon" alt="Icon CollabCode">
             </div>
         `;
     };
