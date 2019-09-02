2822036fa7a52dabbb7f88b339ec3fdd6dce7f6c
JuniorAlvesbr <jrnalves@gmail.com>
2019-09-02 16:49:11 -0300

    Adicionado a imgem eye.png por css no lugar do texto

--- src/objects/eye-collabcode/index.js
+++ src/objects/eye-collabcode/index.js
@@ -2,11 +2,32 @@ const eyeCollabcode = (function () {
 
     const module = {};
 
-    module._style = () => { };
+    module._style = () => {
+        const $head = document.querySelector('head');
+        const $style = document.createElement('style');
 
-    module.render = () => `
-        <label>Mostrar Senha</label>
-    `;
+        $style.textContent = `
+            .eye-collabcode {
+                display: block;
+                text-indent: -9999px;
+                background-image: url("../img/eye.png");
+                background-repeat: no-repeat;
+                background-position: center;
+                width: 24px;
+                height: 15px;
+            }
+        `;
+
+        $head.insertBefore($style, null);
+    };
+
+    module.render = () => {
+        module._style();
+
+        return `
+            <label class=eye-collabcode>Mostrar Senha</label>
+        `;
+    };
 
     return {
         render: module.render
