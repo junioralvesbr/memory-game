b54e307997830d774639c40cf05ef384a6b0c8f8
JuniorAlvesbr <jrnalves@gmail.com>
2019-09-23 11:51:47 -0300

    Inserido o botão reinicar no topo da tela de fim de jogo

--- src/objects/layer-end/index.js
+++ src/objects/layer-end/index.js
@@ -8,26 +8,40 @@ const layerEnd = (function () {
 
         $style.textContent = `
             .layer-end {
+                width: 100%;
+                height: 100%;
                 position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
+                top: 0;
+                background-color: rgba(0,0,0,0.2);
                 display: none;
             }
 
             .layer-end.-active {
                 display: block;
             }
+            
+            .layer-end .img {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+            }
+
         `;
 
         $head.insertBefore($style, null);
     };
 
+    
     module.render = () => {
+        const $backButton = backButton.render("Restart", "game");
         module._style();
 
         return `
-            <img class="layer-end" src="img/tenor.jpg" alt=Troféu>
+            <div class="layer-end">
+                <img class="img" src="img/tenor.jpg" alt=Troféu>
+                ${$backButton}
+            </div>
         `;
     };
 
